d58f639c180bb568d3243fc5b8ea00f5f2edcd61
Magnus Gyllenswärd <magnus.gyllensward@adlibris.com>
2025-12-08 14:07:59 +0100

    REDESIGN 1

--- config.rb
+++ config.rb
@@ -1,10 +1,36 @@
 helpers do
-  def auto_image_tag(filename)
-    alt_text = filename
-                .gsub(File.extname(filename), '')
-                .gsub(/[-_]/, ' ')
-                .capitalize
+  def auto_image_tag(filename, alt_text = nil)
+    # Use provided alt text or generate from filename
+    if alt_text.nil?
+      alt_text = generate_alt_text(filename)
+    end
     image_tag(filename, alt: alt_text)
+  end
+
+  private
+
+  def generate_alt_text(filename)
+    # Improved alt text generation with project-specific descriptions
+    project_descriptions = {
+      'mindler-concept1' => 'Mindler social anxiety app concept interface design',
+      'aerocrine2' => 'Aerocrine Niox Vero medical device project showcase',
+      '6_3' => 'Future healthcare concept design by Veryday',
+      'insight-pump2' => 'Roche Diagnostics insulin pump interface design',
+      '34' => 'Swegon air handling unit interface design project',
+      '39_2' => 'ITT Flygt pump station interface design',
+      '29_6' => 'Sonicspree music game design by Veryday',
+      '27' => 'Spotify design project showcase',
+      'mindler' => 'Mindler company logo',
+      'tvnu' => 'TV.nu company logo',
+      'nyarp' => 'Nyarp company logo',
+      'adlibris' => 'Adlibris company logo'
+    }
+
+    filename_without_ext = filename.gsub(File.extname(filename), '')
+    
+    # Return specific description if available, otherwise generate generic one
+    project_descriptions[filename_without_ext] || 
+      "#{filename_without_ext.gsub(/[-_]/, ' ').split.map(&:capitalize).join(' ')} project image"
   end
 end
 
